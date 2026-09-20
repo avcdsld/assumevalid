6cfee29a479166743bc038a198ad968397cb5217
Zeroichi Arakawa <arandoros@gmail.com>
2026-09-20 14:49:07 +0900

    show the assumevalid chain in bitcoin-cli -netinfo

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -426,6 +426,8 @@ private:
     std::string ChainToString() const
     {
         switch (gArgs.GetChainType()) {
+        case ChainType::ASSUMEVALID:
+            return " assumevalid";
         case ChainType::TESTNET4:
             return " testnet4";
         case ChainType::TESTNET:
